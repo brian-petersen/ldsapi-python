6977755c4e6b6793606902bbc95a61abfa2fcc08
Brian Petersen <skiwalk3r@gmail.com>
2018-08-08 07:23:00 -0600

    Begins refactoring project

--- setup.py
+++ setup.py
@@ -1,48 +1,64 @@
-#!/usr/bin/env python
+import io
 import os
-from setuptools import setup  # , find_packages
 
-BASEDIR = os.path.dirname(__file__)
-__version__ = '0'
-# Now lets got find the real version number
-for _ in open(os.path.join(BASEDIR, 'lds_org.py')).readlines():
-    if _.startswith('__version__'):
-        exec(_.strip(), None)
-        break
-else:
-    raise RuntimeError("Unable to find __version__ in lds_org.py")
+from setuptools import find_packages, setup
 
-requirements = [
+# Package meta-data.
+NAME = 'ldsorg'
+DESCRIPTION = 'Access LDS.org unit information'
+URL = 'https://github.com/brian-petersen/ldsorg-python'
+EMAIL = 'spambrianp@gmail.com'
+AUTHOR = 'Brian Petersen'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
     'requests',
-    'certifi'
 ]
 
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        LONG_DESCRIPTION = '\n' + f.read()
+except FileNotFoundError:
+    LONG_DESCRIPTION = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+ABOUT = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), ABOUT)
+else:
+    ABOUT['__version__'] = VERSION
+
 setup(
-    name='LDS-org',
-    author='Clinton James',
-    author_email='clinton.james@anuit.com',
-    url='https://www.github.com/jidn/lds-org/',
-    download_url='https://github.com/jidn/lds-org/tarball/' + __version__,
-    description='Access LDS.org json information',
-    long_description=open('README.md').read(),
-    license='MIT License',
-    version=__version__,
-    keywords=['lds'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.7',
-    ],
-    py_modules=['lds_org'],
-    # packages=find_packages(exclude=['tests']),
-    zip_safe=False,
+    name=NAME,
+    version=ABOUT['__version__'],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=[NAME],
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     include_package_data=True,
-    install_requires=requirements,
-    # Install these with "pip install -e '.[paging]'" or '.[docs]'
-    # extras_require={
-    #     'docs': 'sphinx',
-    # }
+    license='MIT',
+    classifiers=[],
 )
